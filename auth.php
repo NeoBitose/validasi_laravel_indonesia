4c85df74d8f69d2ab4af478a0354057f3feba3a6
NeoBitose <ramadhanalif430@gmail.com>
2023-01-31 02:33:27 +0700

    init code

---
+++ auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Data pengguna tidak cocok.',
+    'password' => 'Password salah',
+    'throttle' => 'Login gagal. Coba lagi nanti.',
+
+];
---
+++ passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password anda telah direset!',
+    'sent' => 'Kami sudah mengirimkan email verifikasi kepada anda!',
+    'throttled' => 'Mohon tunggu sebelum mencoba lagi.',
+    'token' => 'Token password ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan email tersebut.",
+
+];
